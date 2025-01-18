fa6c13d073adab244527453f51fba08081f7e999
John Adrian Cruz <jhndrncrz@gmail.com>
2025-01-19 00:24:49 +0800

    feat(ui): add minimal Mantine example
    added minimal example of Mantine usage using AppShell component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,29 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+
+import { App } from './App.tsx'
+
+import '@mantine/core/styles.css'
+import '@mantine/dates/styles.css'
+import '@mantine/charts/styles.css'
+import '@mantine/notifications/styles.css'
+import '@mantine/code-highlight/styles.css'
+import '@mantine/tiptap/styles.css'
+import '@mantine/dropzone/styles.css'
+import '@mantine/carousel/styles.css'
+import '@mantine/spotlight/styles.css'
+import '@mantine/nprogress/styles.css'
+
+import { createTheme, MantineProvider } from '@mantine/core'
+
+const theme = createTheme({
+  /** Put your mantine theme override here */
+});
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+    <StrictMode>
+        <MantineProvider theme={theme}>
+            <App />
+        </MantineProvider>
+    </StrictMode>,
 )
